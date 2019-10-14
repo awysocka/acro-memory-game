e0fde78ca8225897cd91ac3a82be6997bbb8065d
awysocka <wysocka.anna88@gmail.com>
2019-10-14 23:42:01 +0200

    Add cards matching

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,29 @@
 
 const cards = document.querySelectorAll('.card');
+let flipedCard = null;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    const clickedCard = this;
+    clickedCard.classList.add('flip');
+
+    if (flipedCard != null) {
+
+        const clickedCardImgSrc = clickedCard.querySelectorAll('.card__img')[0].src;
+        const flipedCardImgSrc = flipedCard.querySelectorAll('.card__img')[0].src;
+
+        if(clickedCardImgSrc === flipedCardImgSrc) {
+            flipedCard = null;
+        } else {
+            setTimeout(function () {
+                flipedCard.classList.remove('flip');
+                clickedCard.classList.remove('flip');
+                flipedCard = null;
+            }, 1000); 
+        }
+
+    } else {
+        flipedCard = clickedCard;
+    }
 }
 
-cards.forEach(card => {
-    console.log(card);
-    card.addEventListener("click", flipCard);
-});
-
+cards.forEach(card => card.addEventListener("click", flipCard));
