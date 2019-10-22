6bf4515a56009d3e17bc8c3d6adcadca7bf3c00d
awysocka <wysocka.anna88@gmail.com>
2019-10-23 00:17:07 +0200

    add and handle time and flips counters

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,103 @@
 const cards = document.querySelectorAll('.card');
-let flipedCard = null;
+const timeCounterElement = document.getElementById('time--counter');
+const flipsCounterElement = document.getElementById('flips--counter');
+const gameOverOverlay = document.getElementById('game-over-text');
+const victoryOverlay = document.getElementById('victory-text');
+const overlays = document.querySelectorAll('.overlay__text');
+let intervalId;
+let flipedCard;
+let timeCounter;
+let flipsCounter;
 
+function startGame() {
+    cards.forEach(card => {
+        card.classList.remove('flip');
+    });
 
-function shuffleCards() {
-    cards.forEach(
-        function (currentCard, currentIndex) {
-            const randomIndex = Math.floor(Math.random() * (currentIndex + 1));
-            cards[randomIndex].style.order = currentIndex;
-            currentCard.style.order = randomIndex;
-        }
-    );
+    flipedCard = null;
+    timeCounter = 50;
+    timeCounterElement.innerText = ' ' + timeCounter;
+    flipsCounter = 0;
+    flipsCounterElement.innerText = ' ' + flipsCounter;
+
+    shuffleCards();
+
+    intervalId = setInterval(decreaseTimeCounter, 1000);
 }
 
-shuffleCards();
+function initGame() {
+    cards.forEach(card => card.addEventListener('click', flipCard));
 
-function flipCard() {
-    const clickedCard = this;
-    clickedCard.classList.add('flip');
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
 
-    if (flipedCard != null) {
+            startGame();
+        });
+    });
+}
 
-        const clickedCardImgSrc = clickedCard.querySelectorAll('.card__img')[0].src;
-        const flipedCardImgSrc = flipedCard.querySelectorAll('.card__img')[0].src;
+function shuffleCards() {
+    cards.forEach(function (currentCard, currentIndex) {
+        const randomIndex = Math.floor(Math.random() * (currentIndex + 1));
+        cards[randomIndex].style.order = currentIndex;
+        currentCard.style.order = randomIndex;
+    });
+}
 
-        if (clickedCardImgSrc === flipedCardImgSrc) {
-            flipedCard = null;
-        } else {
-            setTimeout(function () {
-                flipedCard.classList.remove('flip');
-                clickedCard.classList.remove('flip');
-                flipedCard = null;
-            }, 1000);
-        }
+function decreaseTimeCounter() {
+    timeCounter--;
+    timeCounterElement.innerText = ' ' + timeCounter;
 
-    } else {
-        flipedCard = clickedCard;
+    if (timeCounter == 0) {
+        gameOverOverlay.classList.add('visible');
+        clearInterval(intervalId);
     }
 }
 
-cards.forEach(card => card.addEventListener("click", flipCard));
+function flipCard() {
+    const clickedCard = this;
+
+    if (!clickedCard.classList.contains('flip')) {
+
+        clickedCard.classList.add('flip');
+
+        if (flipedCard != null) {
+            const clickedCardImgSrc = clickedCard.querySelectorAll('.card__img')[0].src;
+            const flipedCardImgSrc = flipedCard.querySelectorAll('.card__img')[0].src;
+
+            if (clickedCardImgSrc === flipedCardImgSrc) {
+                flipedCard = null;
+
+                allFlipedCards = document.querySelectorAll('.flip');
+
+                if (allFlipedCards.length == cards.length) {
+                    victoryOverlay.classList.add('visible');
+                    clearInterval(intervalId);
+                }
+               
+            } else {
+                setTimeout(function () {
+                    flipedCard.classList.remove('flip');
+                    clickedCard.classList.remove('flip');
+                    flipedCard = null;
+                }, 1000);
+            }
+        } else {
+            flipedCard = clickedCard;
+        }
+
+        increaseFlipsCounter();
+    }
+}
+
+function increaseFlipsCounter() {
+    flipsCounter++;
+    flipsCounterElement.innerHTML = ' ' + flipsCounter;
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initGame());
+} else {
+    initGame();
+}
